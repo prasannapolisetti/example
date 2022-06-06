a391b47de2e0ead50d124d8c58b0c67f5d8df8a4
ppolisetti <ppolisetti@host.docker.internal>
2022-06-06 20:47:57 +0530

    first commit

---
+++ threadsexample/src/main/java/com/springecommerceapplication/ThreadsexampleApplication.java
@@ -0,0 +1,13 @@
+package com.springecommerceapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadsexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadsexampleApplication.class, args);
+	}
+
+}
